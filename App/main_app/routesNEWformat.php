4beb741494a7b3854cadf49268ba553d9979f213
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-13 08:19:53 +0300

    Update routesNEWformat.php
    edit new routes

--- App/main_app/routesNEWformat.php
+++ App/main_app/routesNEWformat.php
@@ -30,58 +30,52 @@ return [
         'methods' => [
             'GET' => [
                 'controller' => [MainApp\Controllers\ContactController::class, 'show'],
-                'needauth' => false,
-                'onlyforguest' => true,
             ],
             'POST' => [
                 'controller' => [MainApp\Controllers\ContactController::class, 'send'],
-                'needauth' => false,
-                'onlyforguest' => true,
             ],
         ],
+        'needauth' => true,
+        'onlyforguest' => false,
     ],
     'profile' => [
         'path' => '/profile',
         'methods' => [
             'GET' => [
                 'controller' => [MainApp\Controllers\ProfileController::class, 'view'],
-                'needauth' => true,
-                'onlyforguest' => false,
             ],
             'POST' => [
                 'controller' => [MainApp\Controllers\ProfileController::class, 'update'],
-                'needauth' => true,
-                'onlyforguest' => false,
             ],
         ],
+        'needauth' => true,
+        'onlyforguest' => false,
     ],
     'admin' => [
         'path' => '/admin',
         'methods' => [
             'GET' => [
                 'controller' => [MainApp\Controllers\AdminController::class, 'dashboard'],
-                'needauth' => true,
-                'onlyforguest' => false,
             ],
             'POST' => [
                 'controller' => [MainApp\Controllers\AdminController::class, 'manage'],
-                'needauth' => true,
-                'onlyforguest' => false,
             ],
         ],
+        'needauth' => true,
+        'onlyforguest' => false,
     ],
     'login' => [
         'path' => '/login',
         'methods' => [
             'GET' => [
                 'controller' => [MainApp\Controllers\AuthController::class, 'showLoginForm'],
-                'needauth' => false,
-                'onlyforguest' => true,
+            'needauth' => true,
+            'onlyforguest' => false,
             ],
             'POST' => [
                 'controller' => [MainApp\Controllers\AuthController::class, 'login'],
-                'needauth' => false,
-                'onlyforguest' => true,
+            'needauth' => true,
+            'onlyforguest' => false,
             ],
         ],
     ],
@@ -90,9 +84,9 @@ return [
         'methods' => [
             'POST' => [
                 'controller' => [MainApp\Controllers\AuthController::class, 'logout'],
-                'needauth' => true,
-                'onlyforguest' => false,
             ],
         ],
+        'needauth' => true,
+        'onlyforguest' => false,
     ],
 ];
