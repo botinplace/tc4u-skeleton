371f61cb88937d7b65fd061f3ada85ad83ff14a2
botinplace <49042010+botinplace@users.noreply.github.com>
2025-02-14 09:47:09 +0300

    Update pagedata.php

--- App/main_app/Config/pagedata.php
+++ App/main_app/Config/pagedata.php
@@ -1,23 +1,29 @@
 // pagedata.php
 return [
     'home'=>[
-        		'id'=>1,
-        		'baseTemplate' => null,
+                'id'=>1,
+                'baseTemplate' => null,
                 'contentFile' => 'Index',        
                 'title' => 'Главная страница',
+                'titleSpare'=>'Альтернативный Заголовок',
                 'description' => 'Описание главной страницы',
+                'keywords'=> null,
                 'isHidden' => false,
         		'parentId' => null,
         		'layer'=>1,
+                'priority'=>0,
             ],
     'about'=>[
-        		'id'=>2,
-        		'baseTemplate' => 'base',
+                'id'=>2,
+                'baseTemplate' => 'base',
                 'contentFile' => 'About',       
                 'title' => 'О нас',
+                'titleSpare'=>'Альтернативный Заголовок',
                 'description' => 'Описание страницы О нас',
+                'keywords'=> null,
                 'isHidden' => false,
-        		'parentId' => null,
-        		'layer'=>1,
+                'parentId' => null,
+                'layer'=>1,
+                'priority'=>0,
             ]
 ];
